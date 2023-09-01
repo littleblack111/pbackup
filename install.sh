3516c4e878388c85158ed852a0c425de9cb6d643
littleblack111 <littleblack11111@gmail.com>
2023-09-01 11:33:15 +0800

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-curl -O https://github.com/littleblack111/pbackup/archive/refs/heads/main.zip
-unzip main.zip || zip -d main.zip
-pushd main
+curl -O https://codeload.github.com/littleblack111/pbackup/zip/refs/heads/main
+unzip main || zip -d main
+pushd pbackup-main
 pip install -r requirements.txt || echo no requirements.txt found
 sudo install main.py /usr/bin/bbackup
 sudo install stdlib.py /usr/bin
+popd
 pbackup
